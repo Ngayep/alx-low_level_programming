bb9e3c87eadbea01f0a166b4b9d0fe9f95fa1bce
Jessica Ngayep <courtesyofjess@gmail.com>
2023-09-14 07:11:19 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,20 +8,22 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = size; j >= 1; j--)
+			for (j = 1; j <= size - 1; j++)
 			{
-				if (i < j)
-					_putchar(' ');
-				else
-					_putchar('#');
+				_putchar(' ');
 			}
-			_putchar('\n');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
 		}
 	}
-	else
-		_putchar('\n');
 }
