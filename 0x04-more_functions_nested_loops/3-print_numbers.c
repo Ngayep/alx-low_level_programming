ed2aff2977149efc048b7ed56678b35615c14ba1
Jessica Ngayep <courtesyofjess@gmail.com>
2023-09-14 04:20:22 -0700

    printnum

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * @void: it doesn't take any parameter
+ * return: it returns 0 if successful
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++;)
+	{
+		_putchar('i');
+	}
+		putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@ more functions and nested loops file
 0-isupper.c checks for uppercase character
 1-isdigit.c is a function that checks for a digit
 2-mul.c is a function that multiplies two integers
+3printnumbers is a file containing a function that prints numbers from 0 to 9
