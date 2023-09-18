4299fbe3f74abfa323a798d4d2a5e51b91a483fe
Jessica Ngayep <courtesyofjess@gmail.com>
2023-09-18 12:05:43 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps the values of two integers
+ *@a: pointer to one of the values to be swapped
+ *@b: pointer to one of the values to be swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,3 @@
 this is to describe the files in this directory
+0resetto98 is a fuction that takes a pointer as parameter and updates the value it points to to 98
+1-swap.c is a function that swaps the value of two integers
