3fc03f99a4a1dfa5f414e68b7329fb82f5720cd5
Jessica Ngayep <courtesyofjess@gmail.com>
2023-09-18 16:50:18 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,11 +18,26 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n - 1; i++)
 	{
+		if (a[i] < 0)
+		{
+			_putchar ('-');
+			_putchar ('0' - a[i] % 10);
+		}
+		else
+		{
 		_putchar (a[i] + '0');
+		}
 		_putchar (',');
 		_putchar (' ');
 	}
-
+	if (a[n - 1] < 0)
+	{
+		_putchar('-');
+		_putchar('0' - a[n - 1] % 10);
+	}
+	else
+	{
 	_putchar (a[n - 1] + '0');
+	}
 	_putchar ('\n');
 }
