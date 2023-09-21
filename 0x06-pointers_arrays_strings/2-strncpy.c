bae4e2d537082dfcb2ba702fc5da2e61d0c889ce
Jessica Ngayep <courtesyofjess@gmail.com>
2023-09-21 15:05:45 -0700

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,24 +5,20 @@
  * @dest: string receiving copy
  * @src: string to be copied
  * @n: limit of destination string
+ * return: returns the coopied string
  */
 
 char *_strncpy(char *dest, char *src, int n);
 {
-	int i = 0;
-	int j = 0;
+	int i;
 
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0' && j < n)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[i] = src[j];
-		j++;
+		dest[i] = src[i];
 	}
 	if (j < n)
 	{
-	dest[i] = '\0';
+		dest[i] = '\0';
 	}
 	return (dest);
 }
