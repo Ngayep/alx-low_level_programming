c32cfd0b48e51d05a9263ef604a6067b9bdc3d62
Jessica Ngayep <courtesyofjess@gmail.com>
2023-10-02 15:32:16 -0700

    numberofarguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file followed by a new line
+ * main - prints the number of arguments passed into it
+ * followed by a new line
  * @argc :  count of arguments supplied to the program
  * @argv : array of pointer to char
  * Return: 0 if success
@@ -9,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	argv[argc] = '\0'; /*added to pass checks*/
+	printf("%d\n", argc);
+	argv[argc] = '\0'; /*cancelling warning*/
 	return (0);
 }
