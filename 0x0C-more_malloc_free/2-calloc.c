be9ce993af92ede915c0f944f04cf5388998ddbf
Jessica Ngayep <courtesyofjess@gmail.com>
2023-10-08 06:57:58 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	void *P = malloc(nmemb * size);
+	void *p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
