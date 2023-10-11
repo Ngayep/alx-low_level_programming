c845d3ba0ac563ddc1c4fca6304758cb1b755e98
Jessica Ngayep <courtesyofjess@gmail.com>
2023-10-10 21:14:26 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,18 +3,16 @@
 
 /**
  * struct dog - attributes of a dog
- * my_struct - TYpedef for struct dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
 */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-dog;
+};
 
 #endif
