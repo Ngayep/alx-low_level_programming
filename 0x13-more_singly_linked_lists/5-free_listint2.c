fb7fb55a2229d5c721ef206accc77b9a7df34cbe
Jessica Ngayep <courtesyofjess@gmail.com>
2023-10-24 00:09:42 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,14 @@ void free_listint2(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
 	{
-		temp = head;
-		head = head->next;
+		temp = *head;
+		*head = *head->next;
 		free(temp);
 	}
-
 	*head = NULL;
 }
