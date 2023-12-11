b24b3cfaf1accb808a249ce08379b5f25928c251
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-11 15:49:10 -0800

    library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-c_files=$(find . -type f -name "*.c")
-for file in $c_files; do
-	gcc -c $file -fPIC -o ${file%.c}.o
-done
-	gcc -shared -o liball.so *.o
-rm -r *.o
+gcc -fPIC -c *c&&gcc -shared -o liball.so *.o
