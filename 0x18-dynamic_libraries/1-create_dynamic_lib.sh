d186c714e215bc1a63f95988297ad22db47177cf
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-11 15:33:17 -0800

    liball

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-gcc -c -fPIC *.c && gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+c_files=$(find . -type f -name "*.c")
+for file in $c_files; do
+	gcc -c $file -fPIC -o ${file%.c}.o
+done
+	gcc -shared -o liball.so *.o
+rm -r *.o
